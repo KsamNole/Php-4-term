cfbb00aa2e5b01fecba13416ab311a646f2a24d9
batyadmx <andronovaleksandrptg@yandex.ru>
2021-05-16 22:58:25 +0500

    watafuck-matahafaka

---
+++ resources/views/auth/signup.blade.php
@@ -0,0 +1,34 @@
+@extends('layout')
+
+@section('main')
+    <h2 style="text-align: center">$-----------------> Регистрация <-----------------$</h2>
+    <form method="POST" action="{{ route('auth.signup') }}" novalidate>
+        @csrf
+        <div class="containers-reg-form">
+            <div>
+                <label for="username">Username</label>
+                <input class="{{ $errors->has('username') ? 'is-invalid' : '' }}" type="text" name="username"
+                       id="username">
+            </div>
+            <div>
+                <label for="password">Password</label>
+                <input class="{{ $errors->has('password') ? 'is-invalid' : '' }}" type="password" name="password"
+                       id="password">
+            </div>
+            @if ($errors->has('username'))
+                <div>
+                    <br>
+                    <span>{{ $errors->first('username') }}</span>
+                </div>
+            @endif
+            @if ($errors->has('password'))
+                <div>
+                    <br>
+                    <span>{{ $errors->first('password') }}</span>
+                </div>
+            @endif
+            <button class="btn-huge" type="submit">Зарегистрироваться</button>
+        </div>
+    </form>
+    @include('particles.footer')
+@endsection
---
+++ resources/views/particles/footer.blade.php
@@ -0,0 +1,8 @@
+<pre style="font-size: 1px; text-align: center">
+        <?php
+    $file = file('anonymous_ascii.txt');
+    foreach ($file as $item) {
+        echo $item;
+    }
+    ?>
+</pre>
