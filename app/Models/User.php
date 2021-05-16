5370688daec6ffb1b1133d721cf62b5077891f9f
Maxim <cvetkov843@gmail.com>
2021-05-16 22:39:53 +0500

    Global convolution

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,14 +11,15 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
+    public $timestamps = false;
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name',
-        'email',
+        'username',
         'password',
     ];
 
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+/* Авторизация */
+
+Route::get('/signup', [\App\Http\Controllers\AuthController::class, 'getSignup'])->name('auth.signup');
+Route::post('/signup', [\App\Http\Controllers\AuthController::class, 'postSignup']);
