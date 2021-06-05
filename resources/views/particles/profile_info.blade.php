49c6038684a5496868f9ad43e45511a21bb72cbb
Maxim <cvetkov843@gmail.com>
2021-06-05 16:32:09 +0500

    Version 0.1

--- resources/views/particles/profile_info.blade.php
+++ resources/views/particles/profile_info.blade.php
@@ -6,10 +6,9 @@
     <h2 style="text-align: center;">{{ $user->getUsername() }}</h2>
     <br>
     @if (Auth::check() && (Auth::user()->isAdmin() || Auth::user()->getUsername() == $user->getUsername()))
-        <form method="POST" action="{{route('delete.page')}}">
+        <form method="POST" action="{{route('delete.page')}}" id="deletePage">
             @csrf
             <input type="hidden" value="{{$user->getUsername()}}" name="name">
-            <button type="submit">Удалить страницу</button>
         </form>
         <p style="text-align: center;"><a class="delete-profile" id="delete" onclick="_delete()" href="#">Удалить
                 страницу</a></p>
@@ -18,7 +17,7 @@
         function _delete() {
             let del = document.getElementById('delete');
             if (del.textContent == 'Уверен?') {
-                del.setAttribute('href', '/profile/delete-page?name={{$user->getUsername()}}')
+                document.getElementById('deletePage').submit();
             }
             del.textContent = 'Уверен?';
         }
