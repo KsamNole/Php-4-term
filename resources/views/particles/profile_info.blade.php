bdfd01f213e9155c5213efc7ee90b3d48d5ab64f
Maxim <cvetkov843@gmail.com>
2021-06-05 14:53:52 +0500

    Fix bugs

--- resources/views/particles/profile_info.blade.php
+++ resources/views/particles/profile_info.blade.php
@@ -1,14 +1,9 @@
 <div class="profile_info">
-    @if ($user->avatar != null)
-        <img class="profile_img" src="{{ asset('/storage/' . $user->avatar) }}" alt="profile_img">
-    @else
-        <img class="profile_img" src="{{url('imgs/default_photo_profile.jpg')}}" alt="profile_img">
-    @endif
+    <a href="@if(Auth::check() && Auth::user()->getUsername() == $user->getUsername()){{route('upload.page')}}@endif">
+        <img class="profile_img" src="@if($user->avatar){{ asset('/storage/' . $user->avatar) }}@else{{url('imgs/default_photo_profile.jpg')}}@endif" alt="profile_img">
+    </a>
     <br>
     <h2 style="text-align: center;">{{ $user->getUsername() }}</h2>
-    @if (Auth::user()->getUsername() == $user->getUsername())
-        <h2 style="margin-left: 50px;"><a href="{{route('upload.page')}}">Загрузить картинку</a></h2>
-    @endif
     <br>
     @if (Auth::user()->isAdmin() || Auth::user()->getUsername() == $user->getUsername())
         <p style="text-align: center;"><a class="delete-profile" id="delete" onclick="_delete()" href="#">Удалить
