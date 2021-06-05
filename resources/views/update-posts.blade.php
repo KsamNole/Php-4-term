7abc49907dae20e96f54fc491669307bfa2324a8
batyadmx <andronovaleksandrptg@yandex.ru>
2021-06-05 15:40:42 +0500

    Update delete func

--- resources/views/update-posts.blade.php
+++ resources/views/update-posts.blade.php
@@ -1,16 +1,15 @@
 @foreach($posts as $post)
     @if($post->author == $user->getUsername())
         <div class="post">
+            <form method="POST" action="{{route('delete.post')}}" id="deletePost-{{$post->id}}">
+                @csrf
+                <input type="hidden" value="{{$post->id}}" name="post_id">
+            </form>
             <div class="time-delete-post">
                 <p style="padding: 5px 0 0 5px; color: white;">{{ $post->created_at }}</p>
                 @if (Auth::check() && (Auth::user()->isAdmin() || Auth::user()->getUsername() == $user->getUsername()))
-                    <form method="POST" action="{{route('delete.post')}}">
-                        @csrf
-                        <input type="hidden" value="{{$post->id}}" name="post_id">
-                        <button type="submit">Удалить пост</button>
-                    </form>
                 <p style="text-align: right; padding-right: 10px;">
-                    <a class="delete" href="/profile/delete-post?post_id={{$post->id}}">
+                    <a class="delete" href="javascript:document.getElementById('deletePost-{{$post->id}}').submit();">
                         <img style="width: 10px; height: 10px;" src="{{ url('imgs/delete.png') }}">
                     </a>
                 </p>
@@ -31,16 +30,15 @@
             @foreach($comments as $comment)
                 @if($comment->id_post == $post->id)
                     <div class="comment">
+                        <form method="POST" action="{{route('delete.comment')}}" id="deleteComment-{{$comment->id}}">
+                            @csrf
+                            <input type="hidden" value="{{$comment->id}}" name="c_id">
+                        </form>
                         <div class="time-delete-post">
                             <p style="padding: 5px 0 0 15px; color: white;">{{ $comment->author }}</p>
                             @if (Auth::check() && (Auth::user()->isAdmin() || Auth::user()->getUsername() == $user->getUsername()))
-                                <form method="POST" action="{{route('delete.comment')}}">
-                                    @csrf
-                                    <input type="hidden" value="{{$comment->id}}" name="c_id">
-                                    <button type="submit">Удалить коммент</button>
-                                </form>
                             <p style="text-align: right; padding-right: 10px;">
-                                <a class="delete" href="/profile/delete-comment?c_id={{$comment->id}}">
+                                <a class="delete" href="javascript:document.getElementById('deleteComment-{{$comment->id}}').submit();">
                                     <img style="width: 10px; height: 10px;" src="{{ url('imgs/delete.png') }}">
                                 </a>
                             </p>
